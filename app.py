0860b22b92a1e81304b2d119b6572820e8789746
SotheaBan <bansothea.morakot@gmail.com>
2025-05-13 13:21:23 +0700

    Second exercies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -6,9 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", name="indexpage") 
 
-@app.route("/about")
-def about():
-    return "This is a simple Flask application."
+#exercise 1. template 
+@app.route("/favorites")
+def favorites():
+    if request.method == "POST":
+        name = request.form.get("username")
+        fruit = request.form.get("fruit")
+        color = request.form.get("color")
+        food = request.form.get("tast")
+        return render_template("resultSubmitionForm.html", name=name, color=color, food=food, fruit=fruit)
+
+    return render_template("form.html", name="favorites")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
